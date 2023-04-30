46daf31e64eb8144709aa974f155ee49dd3b6515
SandwichDream <karnll.adnitorll@gmail.com>
2023-04-30 14:41:59 +0300

    hw-10-23032023-KyrMov

---
+++ blocks_moving.js
@@ -0,0 +1,30 @@
+window.onload = ()=>{
+    let player = document.querySelector(".player");
+    let area = document.querySelector(".area");
+    let PositionX = 0;
+    let PositionY = 0;
+    let step = 1;
+    
+    let playerWidth = Number(window.getComputedStyle(player).getPropertyValue("width").replace("px",""));
+    let areaWidth = Number(window.getComputedStyle(area).getPropertyValue("width").replace("px",""));
+    let playerHeight = Number(window.getComputedStyle(player).getPropertyValue("height").replace("px",""));
+    let areaHeight = Number(window.getComputedStyle(area).getPropertyValue("height").replace("px",""));
+
+    document.addEventListener("keydown", function(event) {
+        if (event.code  === "ArrowLeft" && PositionX > 0) {
+            PositionX -= step;
+            player.style.left = PositionX + "px";
+        } else if (event.code  === "ArrowRight" && PositionX < areaWidth-playerWidth) {
+            PositionX += step;
+            player.style.left = PositionX + "px";
+        } else if (event.code  === "ArrowUp" && PositionY > 0) {
+            PositionY -= step;
+            player.style.top = PositionY + "px";
+        } else if (event.code  === "ArrowDown" && PositionY < areaHeight-playerHeight) {
+            PositionY += step;
+            player.style.top = PositionY + "px";
+        }
+    });
+
+    let images = [];
+}
